e692839d886ed248a6f32e301fdd942122c96430
sabbir <mdsabbirrahmanm740@gmail.com>
2023-08-02 10:30:14 +0600

    fast commit

---
+++ main.js
@@ -0,0 +1,47 @@
+
+
+// array-length
+// let name=['sabbir','rakib','sakib','shohan','razu'];
+// console.log(name.length);
+
+// array index
+// let name=['sabbir','rakib','sakib','shohan','razu'];
+// console.log(name[4]);
+
+
+// array value change index number diya
+// let name=['sabbir','rakib','sakib','shohan','razu'];
+// console.log(name[4]='tamim');
+// console.log(name);
+
+// array last item add kora
+// let name=['sabbir','rakib','sakib','shohan','razu'];
+// name.push('jakir','pasha');
+// console.log(name);
+
+// array last item remove kora
+// let name=['sabbir','rakib','sakib','shohan','razu'];
+// name.pop();
+// console.log(name);
+
+// array fast item remove kora
+// let name=['sabbir','rakib','sakib','shohan','razu'];
+// name.shift();
+// console.log(name);
+
+
+// array fast item add kora
+// let name=['sabbir','rakib','sakib','shohan','razu'];
+// name.unshift('kamal','rafiq');
+// console.log(name);
+
+
+
+// array name diya index ber kora
+// let name=['sabbir','rakib','sakib','shohan','razu'];
+// name.indexOf('sakib');
+
+// array index number diya jekonu value k remove kora apni koto theke porjonto remove korben tar index number dite hobe 1-4
+let name=['sabbir','rakib','sakib','shohan','razu','tamim'];
+name.splice(1,4);
+console.log(name)
